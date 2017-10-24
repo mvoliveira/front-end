b763fc69c584098341d7239f26256eefa7052513
Matheus <matheus.mvo10@gmail.com>
2017-10-24 14:41:35 -0200

    adiciona route para index

--- apps.js
+++ apps.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendfile('index.html');
+
 })
 
 app.listen(3000, function () {
